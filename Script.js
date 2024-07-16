fef52895c1b05557c8e529c4f34c55b4f19c63fb
mohansivakumar359@gmail.com <mohansivakumar>
2024-07-16 20:46:44 +0530

    TASK-5-JS-RE-SUBMIT

---
+++ Script.js
@@ -0,0 +1,60 @@
+let myResume={
+ "basics": {
+   "name": "MOHAN SIVAKUMAR",
+   "email": "mohansivakumar359@gamil.com",
+   "phone": 8056229816,
+   "degree": "DME",
+   "location": {
+     "address": "30 Chinna Sayakara Street",
+     "postalCode": 632301,
+     "city": "Arani",
+     "state": "Tamilnadu",
+     "country": "India"
+   },
+   "profiles": [
+     {
+       "website": "https://www.linkedin.com/in/mohan-s-b3997912b//",
+       "github":"https://github.com/mohansivakumar1994"
+     }
+   ]
+ },
+ "work": [
+   {
+     "company": "Psa Automobiles Pvt.Ltd",
+     "position": "Quality Engg",
+     "startDate": "2020-11-04",
+     "endDate": "2023-08-15",
+     "summary": "i am the Quality Engg handling various quality defect and control lots of experience",
+   },
+ ],
+ "education": [
+   {
+     "institution": "Dusi Polytechnic College",
+     "department": "Mechanical",
+     "studyType": "fulltime",
+     "batch start year": 2013,
+     "batch end year": 2015,
+     "gpa": 7.3,
+   }
+ ],
+ "skills": [
+   {
+     "name": "python,javascript",
+     "level": "beginner",
+     "project": [
+       "automatic attendance maintaining system using python language"
+     ]
+   }
+ ],
+ "languages": [
+   {
+     "language": "Tamil,English",
+   }
+ ],
+ "interests": [
+   {
+     "name": "script writter , youtuber ,"
+   }
+ ]
+}
+console.log(myResume);
